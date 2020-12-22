0c6429a0983df74e560e2883b345090e490f4a99
Rob Williams <rwilliams@spotify.com>
2020-12-21 20:14:58 -0500

    implement config

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
+mod config;
 mod models;
 
 use crate::models::Status;
-use actix_web::{HttpServer, App, web, Responder};
+use actix_web::{web, App, HttpServer, Responder};
+use dotenv::dotenv;
 use std::io;
 
 async fn status() -> impl Responder {
-    web::HttpResponse::Ok()
-        .json(Status{ status: "all good".to_owned() })
+    web::HttpResponse::Ok().json(Status {
+        status: "all good".to_owned(),
+    })
 }
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
-    println!("Starting web server on http://127.0.0.1:8000 ...");
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(status))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    dotenv().ok();
+
+    let config = crate::config::Config::from_env().expect("Problem loading config");
+
+    let bind_host_port = format!("{}:{}", config.server.host, config.server.port);
+    println!("Starting web server on http://{} ...", bind_host_port);
+    HttpServer::new(|| App::new().route("/", web::get().to(status)))
+        .bind(bind_host_port)?
+        .run()
+        .await
 }
