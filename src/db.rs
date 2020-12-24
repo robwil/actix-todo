b71313e7d4c8e0ecd227090ca735426a585f0f54
Rob Williams <rwilliams@spotify.com>
2020-12-24 11:43:15 -0500

    setup basic logging and better error handling

--- src/db.rs
+++ src/db.rs
@@ -1,19 +1,29 @@
 use crate::models::{TodoItem, TodoList};
 use deadpool_postgres::Client;
-use std::io;
 use tokio_pg_mapper::FromTokioPostgresRow;
 
-pub async fn get_todos(client: &Client) -> Result<Vec<TodoList>, io::Error> {
-    // TODO: don't love the error handling here (or lack thereof)
-    let statement = client.prepare("select * from todo_list").await.unwrap();
-    let todos = client
+pub async fn get_todos(client: &Client) -> anyhow::Result<Vec<TodoList>> {
+    let statement = client.prepare("select * from todo_list order by id desc").await?;
+    let lists = client
         .query(&statement, &[])
-        .await
-        .expect("Error getting todo lists")
+        .await?
         .iter()
         // TODO: don't love the error handling here (or lack thereof)
         .map(|row| TodoList::from_row_ref(&row).unwrap())
         .collect::<Vec<TodoList>>();
 
-    Ok(todos)
+    Ok(lists)
+}
+
+pub async fn get_items(client: &Client, list_id: i32) -> anyhow::Result<Vec<TodoItem>> {
+    let statement = client.prepare("select * from todo_item where list_id = $1 order by id asc").await?;
+    let items = client
+        .query(&statement, &[&list_id])
+        .await?
+        .iter()
+        // TODO: don't love the error handling here (or lack thereof)
+        .map(|row| TodoItem::from_row_ref(&row).unwrap())
+        .collect::<Vec<TodoItem>>();
+
+    Ok(items)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod db;
 mod handlers;
 mod models;
 
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use dotenv::dotenv;
 use std::io;
@@ -10,22 +11,30 @@ use tokio_postgres::NoTls;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
+    // Load configuration
     dotenv().ok();
-
     let config = crate::config::Config::from_env().expect("Problem loading config");
 
+    // Create DB pool
     let pool = config
         .pg
         .create_pool(NoTls)
         .expect("failed to connect to database");
 
+    // Config logger
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    // Start up web server
     let bind_host_port = format!("{}:{}", config.server.host, config.server.port);
     println!("Starting web server on http://{} ...", bind_host_port);
     HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .data(pool.clone())
             .route("/", web::get().to(handlers::status))
             .route("/todos{_:/?}", web::get().to(handlers::get_todos))
+            .route("/todos/{list_id}/items{_:/?}", web::get().to(handlers::get_items))
     })
     .bind(bind_host_port)?
     .run()
